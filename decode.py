de186c76ac2f3d364b451b6492af41e7fce91420
Tanmay Aeron <2019csb1124@iitrpr.ac.in>
2021-03-20 10:31:24 +0530

    extended to all formats

--- decode.py
+++ decode.py
@@ -19,7 +19,8 @@ def decode(code):
     if len(df['id'])==0:
         return 0
     else:
-        format = df['format'][0]
+        format = list(df['format'])[0]
+
         if format=='R':
             funct3 = '0b'+machine_code[-15:-12]
             funct7 = '0b'+machine_code[:7]
@@ -35,6 +36,62 @@ def decode(code):
                 fields['rs1']=machine_code[-20:-15]
                 fields['rs2']=machine_code[-25:-20]
                 fields['rd']=machine_code[-12:-7]
+
+
+        elif format=='I':
+            funct3 = '0b'+machine_code[-15:-12]
+            df = df[df.funct3==funct3]
+            if len(df['id'])==0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rd']=machine_code[-12:-7]
+                fields['immediate']=machine_code[0:12]
+
+
+        elif format=='S':
+            funct3 = '0b'+machine_code[-15:-12]
+            df = df[df.funct3==funct3]
+            if len(df['id'])==0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rs2']=machine_code[-25:-20]
+                fields['immediate']=machine_code[0:7]+machine_code[-12:-7]
+
+
+        elif format=='SB':
+            funct3 = '0b'+machine_code[-15:-12]
+            df = df[df.funct3==funct3]
+            if len(df['id'])==0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rs2']=machine_code[-25:-20]
+                fields['immediate']=machine_code[0]+machine_code[-8]+machine_code[1:7]+machine_code[-12:-8]
+
+
+        elif format=='U':
+            fields['neumonic'] = list(df['neumonic'])[0]
+            fields['opcode']=opcode
+            fields['immediate']=machine_code[0:20]
+            fields['rd']=machine_code[-12:-7]
+
+
+        elif format=='UJ':
+            fields['neumonic'] = list(df['neumonic'])[0]
+            fields['opcode']=opcode
+            fields['immediate']=machine_code[0]+machine_code[-20:-12]+machine_code[-21]+machine_code[1:11]  #not shifted 12 bits see later
+            fields['rd']=machine_code[-12:-7]
         return fields
 
 print(decode('00A4E433'))

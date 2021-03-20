7fe5a1c95d781d9a869fce596f72c5f8b9320e3c
Aneeket <aneeketmangal@gmail.com>
2021-03-20 08:31:55 +0530

    added Register view in UI and added few functions in register.py

--- GUI/mainPage.py
+++ GUI/mainPage.py
@@ -5,47 +5,54 @@ from PyQt5.QtWidgets import *
 import sys
 import qtawesome as iconPack
 import qdarkstyle
-class UiComponents():
+from register_file import RegisterFile
+class UiComponents(RegisterFile):
     def __init__(self):
         super().__init__()
+        self.fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        self.fixedfont.setPointSize(14)
         
         
-    def buttonTile(self):
+    def buttonTile(self, name):
         button = QPushButton()
-        button.setText("save")
+        button.setText(name)
         button.setFixedHeight(30)
         button.setFixedWidth(50)
-        button.clicked.connect(lambda: self.fileSave())
+        
         return button
 
 
     def mainLabel(self, top, left, height, width):
-        main_label_text = self.buttonTile()
-        main_label_text.setContentsMargins(10,10,20,10)
+        save_button = self.buttonTile("Save")
+        save_button.clicked.connect(lambda: self.fileSave())
+        save_button.setContentsMargins(10,10,20,10)
+        
         main_label_image = QLabel()
         main_label_image_pixmap = QPixmap("GUI/Images/logo.png")
         main_label_image_pixmap = main_label_image_pixmap.scaled(400, 60)
         main_label_image.setPixmap(main_label_image_pixmap)
         main_label_hBox = QHBoxLayout()
         main_label_hBox.addWidget(main_label_image)
-        main_label_hBox.addWidget(main_label_text)
+        main_label_hBox.addWidget(save_button)
         main_label_hBox.setContentsMargins(10, 10, 10, 10)
         return main_label_hBox
 
 
     def editor(self):
+        self.editorScroll = QScrollArea()
         self.editorlayout = QVBoxLayout()
         self.editorScreen = QPlainTextEdit()
-        fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
-        fixedfont.setPointSize(14)
-        self.editorScreen.setFont(fixedfont)
+        
+        self.editorScreen.setFont(self.fixedfont)
         self.path = 'test/test1.mc'
         self.editorlayout.addWidget(self.editorScreen)
         self.container = QGroupBox("Editor")
         self.container.setLayout(self.editorlayout)
         self.fileOpen()
         self.editorScreen.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-
+        self.editorScroll.setWidget(self.container)
+        self.editorScroll.setWidgetResizable(True)
+        
     def fileOpen(self):
         path = self.path
         f = open(path, 'r')
@@ -61,15 +68,20 @@ class UiComponents():
         self.path = path
 
     def memoryDisplay(self):
-        self.displayWidget = QGroupBox("Memory Lookup")
+        self.scroll = QScrollArea()
+        self.scroll.setFrameShape = None
+        self.displayWidget = QGroupBox()
         vbox = QGridLayout()
         for i in range(10):
             label = QLabel("Ox"+'0'*7+str(i+1))
+            label.setFont(self.fixedfont)
+            
             vbox.addWidget(label, i, 0)
             for j in range(4):
                 temp = QLabel()
                 temp.setText("a")
                 
+                temp.setFont(self.fixedfont)
                 temp.setFixedHeight(40)
                 temp.setFixedWidth(40)
                 temp.setStyleSheet("border :1px solid white;")
@@ -77,8 +89,46 @@ class UiComponents():
                 vbox.addWidget(temp, i, j+1)
                    
         self.displayWidget.setLayout(vbox)
-        self.displayWidget.setContentsMargins(10,10,10,10)
+        self.scroll.setWidget(self.displayWidget)
+        self.scroll.setWidgetResizable(True)
 
+
+    def tabbedView(self):
+        self.tabs = QTabWidget() 
+        self.tab1 = self.scroll1
+        self.tab2 = self.scroll
+        self.tabs.addTab(self.tab1, "Registers") 
+        self.tabs.addTab(self.tab2, "Memory") 
+        
+    
+    
+    def registerDisplay(self):
+        self.scroll1 = QScrollArea()
+        self.scroll1.setFrameShape = None
+        self.displayWidget = QGroupBox()
+        vbox = QGridLayout()
+        for i in range(32):
+            label = QLabel("x"+str(i))
+            
+            vbox.addWidget(label, i, 0)
+            label2 = QLabel("("+ self.get_alt_name(i)+")")
+            label2.setFont(self.fixedfont)
+            vbox.addWidget(label2, i, 1)
+            temp = QLabel()
+            currValue = self.get_register(i)
+            temp.setText("0x"+currValue)
+            
+            temp.setFont(self.fixedfont)
+            label.setFont(self.fixedfont)
+            temp.setFixedHeight(40)
+            temp.setFixedWidth(160)
+            temp.setStyleSheet("border :1px solid white;")
+            temp.setAlignment(QtCore.Qt.AlignCenter) 
+            vbox.addWidget(temp, i, 2)
+                   
+        self.displayWidget.setLayout(vbox)
+        self.scroll1.setWidget(self.displayWidget)
+        self.scroll1.setWidgetResizable(True)
 
 class mainScreen(QWidget, UiComponents):
     def __init__(self):
@@ -105,10 +155,12 @@ class mainScreen(QWidget, UiComponents):
         logo_label = self.mainLabel(top=100, left=100, width=200, height=50)
         
         self.editor()
-        feed = self.container
+        feed = self.editorScroll
         
         self.memoryDisplay()
-        memoryDisplay = self.displayWidget
+        self.registerDisplay()
+        self.tabbedView()
+        memoryDisplay = self.tabs
         hbox = QHBoxLayout()
         hbox.addWidget(feed,10)
         hbox.addWidget(memoryDisplay, 4)
--- register_file.py
+++ register_file.py
@@ -14,17 +14,25 @@ class RegisterFile:
 
     def __init__(self):
         self._registers = []
+        self.initialise_registers()
+        self._alt_name = ["zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0/fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"]
 
     def initialise_registers(self):
         self._registers = ["0" * 8] * 32
         self._registers[2] = "7ffffff0"
         self._registers[3] = "1" + "0" * 7
+        
+    
 
     def print_registers(self):
         print(self._registers)
 
     def get_register(self,index):
         return self._registers[index]
+    
+    def get_alt_name(self, index):
+        return self._alt_name[index]
+    
 
     def set_register(self,index,data):
         if index==0 and data!="0"*8:

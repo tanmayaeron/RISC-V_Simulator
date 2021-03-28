4f6b7321fa60e75a42ec6217aa09a2a4c875e9b1
Het <62541263+HETFADIA@users.noreply.github.com>
2021-03-29 00:35:22 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -11,12 +11,15 @@ class Memory:
     """
 
     def __init__(self):
-        self.__memory = defaultdict(lambda: "00") # only relevant data stored
+        self.__memory = defaultdict(lambda: "00")  # only relevant data stored
+
+    def print_memory(self):
+        print(self.__memory)
 
     def hexToDec(self, address):
         address_in_dec = int(address, 16)
         return address_in_dec
-    
+
     def getMemoryDisplay(self, address):
         l = []
         """
@@ -32,37 +35,17 @@ class Memory:
             data = self.load_byte(address_in_hex)
             l.append(data)
         return l
-        
-
 
     def load_byte(self, address):
         """
-
         :param address:byte address str of length 8(without 0x)
         :return:1byte data stored at address in hex, str of length 2(without 0x)
         """
-        if address in self.__memory.keys():
-            return self.__memory[address]
-        else:
-            return "00"
 
-    def load_word(self, address):
-        """
-
-        :param address: byte address str of length 8(without 0x)
-        :return: 1word data stored at address, in hex, str of length 8(without 0x)
-        """
-        address_in_dec = self.hexToDec(address)
-        data = ""
-        for i in reversed(range(4)):
-            address_in_hex = "0" * 8 + hex(address_in_dec + i)[2:]
-            address_in_hex = address_in_hex[-8:]
-            data = data + self.load_byte(address_in_hex)
-        return data
+        return self.__memory[address]
 
     def load_halfword(self, address):
         """
-
         :param address: byte address str of length 8(without 0x)
         :return: halfword data stored at address, in hex, str of length 4(without 0x)
         """
@@ -81,10 +64,16 @@ class Memory:
            data: data to be stored at address, str of appropriate size(without 0x)
     """
 
+    def make_length(self, data, length):
+        data = "0"*length+data
+        return data[-length:]
+
     def store_byte(self, address, data):
+        data = self.make_length(data, 2)
         self.__memory[address] = data
 
     def store_word(self, address, data):
+        data = self.make_length(data, 8)
         address_in_dec = self.hexToDec(address)
         for i in range(4):
             address_in_hex = "0" * 8 + hex(address_in_dec + i)[2:]
@@ -92,12 +81,16 @@ class Memory:
             self.store_byte(address_in_hex, data[6 - 2 * i:8 - 2 * i])
 
     def store_halfword(self, address, data):
+        data = self.make_length(data, 4)
         address_in_dec = self.hexToDec(address)
         for i in range(2):
             address_in_hex = "0" * 8 + hex(address_in_dec + i)[2:]
             address_in_hex = address_in_hex[-8:]
             self.store_byte(address_in_hex, data[2 - 2 * i:4 - 2 * i])
 
-# mem = Memory()
-# mem.store_byte("00000000","0A")
-# print(mem.load_word("00000000"))
+
+mem = Memory()
+mem.store_word("10000000", "AAA")
+
+print("dictionary is :", end="")
+mem.print_memory()

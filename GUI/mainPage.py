a7f89c0dd55fe819a0f0c9720b08784fbb0de1fb
Aneeket <aneeketmangal@gmail.com>
2021-03-30 19:56:46 +0530

    removed unused imports

--- GUI/mainPage.py
+++ GUI/mainPage.py
@@ -3,7 +3,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
-import qtawesome as iconPack
 import qdarkstyle
 from register import RegisterFile
 from memory import Memory

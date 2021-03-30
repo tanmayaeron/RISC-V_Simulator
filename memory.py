97a9bd4b91923286c11ac64f521e897f86f4fca1
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2021-03-30 19:33:46 +0530

    Update memory.py
    PMI added

--- memory.py
+++ memory.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 
 
+def make_length(data, length):
+    data = "0"*length+data
+    return data[-length:]
+
 class Memory:
 
     """
-    represents memory in dictionary
+    represents memory in default dictionary
     each key value pair is of form byte-address:data(one byte of data)
     keys - address in hex (without 0x) represented as string of fixed size 8
     values - 1 byte of data in hex(without 0x) represented as string of size 2
@@ -39,7 +43,7 @@ class Memory:
     def load_byte(self, address):
         """
         :param address:byte address str of length 8(without 0x)
-        :return:1byte data stored at address in hex, str of length 2(without 0x)
+        :return:1byte data stored at address, in hex, str of length 2(without 0x)
         """
 
         return self.__memory[address]
@@ -56,7 +60,7 @@ class Memory:
             address_in_hex = address_in_hex[-8:]
             data = data + self.load_byte(address_in_hex)
         return data
-    
+
     def load_word(self, address):
         """
         :param address: byte address str of length 8(without 0x)
@@ -69,18 +73,21 @@ class Memory:
             address_in_hex = address_in_hex[-8:]
             data = data + self.load_byte(address_in_hex)
         return data
-
+    """
     
-    def make_length(self, data, length):
-        data = "0"*length+data
-        return data[-length:]
+    store functions
+    :param address: byte address str of length 8(without 0x)
+           data: data to be stored at address, str of appropriate size(without 0x)
+    """
+
+
 
     def store_byte(self, address, data):
-        data = self.make_length(data, 2)
+        data = make_length(data, 2)
         self.__memory[address] = data
 
     def store_word(self, address, data):
-        data = self.make_length(data, 8)
+        data = make_length(data, 8)
         address_in_dec = self.hexToDec(address)
         for i in range(4):
             address_in_hex = "0" * 8 + hex(address_in_dec + i)[2:]
@@ -88,16 +95,55 @@ class Memory:
             self.store_byte(address_in_hex, data[6 - 2 * i:8 - 2 * i])
 
     def store_halfword(self, address, data):
-        data = self.make_length(data, 4)
+        data = make_length(data, 4)
         address_in_dec = self.hexToDec(address)
         for i in range(2):
             address_in_hex = "0" * 8 + hex(address_in_dec + i)[2:]
             address_in_hex = address_in_hex[-8:]
             self.store_byte(address_in_hex, data[2 - 2 * i:4 - 2 * i])
 
+if __name__=='__main__':
+    mem = Memory()
+    mem.store_word("10000000", "AAA")
 
-mem = Memory()
-mem.store_word("10000000", "AAA")
+    print("dictionary is :", end="")
+    mem.print_memory()
 
-print("dictionary is :", end="")
-mem.print_memory()
+
+class PMI:
+    def __init__(self):
+        self.__MDR = "0"*8
+        self.__MAR = "0"*8
+        self.__memory = Memory()
+
+    def getMDR(self):
+        return self.__MDR
+    
+    def getMAR(self):
+        return self.__MAR
+
+    def setMDR(self,data):
+        data = make_length(data,8)
+        self.__MDR = data
+        
+    def setMAR(self,address):
+        address = make_length(address,8)
+        self.__MAR = address
+
+    def getData(self,size):
+
+        """
+        size : 0 - byte
+               1 - halfword
+               2 - word
+        """
+
+        getArray = [self.__memory.load_byte, self.__memory.load_halfword, self.__memory.load_word]
+        data = getArray[size](self.__MAR)
+        data = make_length(data,8)
+        self.__MDR = data
+                
+    
+    def storeData(self,size):
+        getArray = [self.__memory.store_byte, self.__memory.store_halfword, self.__memory.store_word]
+        data = getArray[size](self.__MAR,self.__MDR)

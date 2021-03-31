ee92a293ec8701764a10fae4b78bffe63dc23eca
Aneeket <aneeketmangal@gmail.com>
2021-03-31 18:34:36 +0530

    updated input.py

--- input.py
+++ input.py
@@ -1,36 +1,29 @@
-from sys import stdin,stdout
-import sys
-from collections import defaultdict
+def make_length(data, length):
+    data = "0"*length+data
+    return data[-length:]
 
-#use
-#import input
-#pass object of Memory class to Input class
-#to be used in main.py
 
-class Input:
+class ReadFile:
 
     def __init__(self):
-        self.code = defaultdict(lambda:"00")
-        #code has all the code lines corresponding to their number in the .mc file
-        #this will be used in 
+        pass 
     
-    def read_mc(self, obj):
-        file = open('test1.mc','r')
+    def read_mc(self, filepath, obj):
+        file = open(filepath,'r')
         flag = 0
         for lines in file:
             if(lines == "$\n"):
-                #data segment to be read after this
                 flag = 1
                 continue
             if(flag == 0):
                 #text segment
-                IC, instruction = map(str, lines.split())
-                IC = int(IC, 16)
-                instruction = int(instruction,16)
-                self.code[IC] = instruction
-                #maps instruction address to individual instruction
-                #may change the instruction to binary to facilitate easier use
+                requiredMemoryLocation, instruction = map(str, lines.split())
+                obj.setMAR(requiredMemoryLocation[2:])
+                obj.setMDR(instruction[2:])
+                obj.storeData(2)
+    
             else:
-                #data segment
-                address, data = map(str, lines.split())
-                obj.store_word(address, data[2:])
+                requiredMemoryLocation, instruction = map(str, lines.split())
+                obj.setMAR(requiredMemoryLocation[2:])
+                obj.setMDR(instruction[2:])
+                obj.storeData(2)

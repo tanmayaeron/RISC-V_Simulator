81fdc4d3e040a0d15a2ade37e4d63eb1dc283a79
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2021-03-31 21:03:02 +0530

    removed redundant flag

--- input.py
+++ input.py
@@ -5,20 +5,11 @@ class ReadFile:
 
     def read_mc(self, filepath, obj):
         file = open(filepath,'r')
-        flag = 0
         for lines in file:
-            if(lines == "$\n"):
-                flag = 1
-                continue
-            if(flag == 0):
-                #text segment
+            try:
                 requiredMemoryLocation, instruction = map(str, lines.split())
                 obj.setMAR(requiredMemoryLocation[2:])
                 obj.setMDR(instruction[2:])
                 obj.storeData(2)
-
-            else:
-                requiredMemoryLocation, instruction = map(str, lines.split())
-                obj.setMAR(requiredMemoryLocation[2:])
-                obj.setMDR(instruction[2:])
-                obj.storeData(2)
+            except:
+                pass

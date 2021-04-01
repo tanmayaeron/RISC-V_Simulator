09dd3f2f7a7bd9061087e167d16cbc0065210f04
Aditya Agarwal <2019csb1064@iitrpr.ac.in>
2021-04-02 02:35:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ run.load_mc()
 while True:
 
     run.fetch()
-    # print(run._IR)
+    print(run._IR)
 
     if run._IR == '0'*8:
         break
     run.decode()
-    # print(run._currOperationId)
+    print(run._currOperationId)
     run.execute()
     run.memoryAccess()
     run.registerUpdate()
-    # run._registerFile.print_registers()
+    run._registerFile.print_registers()
 
 run._PMI.print_memory()
 print("Data in output.txt printed")

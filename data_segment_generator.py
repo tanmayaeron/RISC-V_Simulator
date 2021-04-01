45da8d8a7c37bba6a52669a6b6266164d36fa36d
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2021-04-02 02:08:29 +0530

    Delete data_segment_generator.py

--- data_segment_generator.py
+++
@@ -1,8 +0,0 @@
-file = open('temp2.txt', 'r')
-out = open('temp3.txt', 'w')
-address = 0x10000000
-for i in file:
-    for j in i.split():
-        j = int(j)
-        out.write(hex(address)+" "+hex(j)+"\n")
-        address += 4

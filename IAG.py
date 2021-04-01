61017b8a092f9d08faf8f9c691196b814a923534
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2021-04-01 09:46:33 +0530

    syntax fix and auipc exclusive function

--- IAG.py
+++ IAG.py
@@ -10,8 +10,7 @@ class IAG(HelperFunctions):
     def __init__(self):
         self._PC = "0"*8
         self._PC_Temp = "0"*8
-        self.__constantoffset = "0"*7+"4"                        #is also kept as 32 bit binary to keep uniformity
-        self._imm = "0"*8                                       #isSignExtended extended to 32bits, stored as binary
+        self.__constantoffset = "0"*7+"4"                        #is also kept as 32 bit binary to keep uniformity                                    #isSignExtended extended to 32bits, stored as binary
         self.output_adder = "0"*8
         self.inputB_adder = "0"*8
         self.output_muxPC = "0"*8
@@ -23,21 +22,21 @@ class IAG(HelperFunctions):
     def getPC_Temp(self):
         return self._PC_Temp
         
-    def muxPC(self,PC_select, RA='0'*8):
+    def muxPC(self,PC_select, RA): 
         if PC_select == 1 :
             self.output_muxPC =  RA
         else:
             self.output_muxPC = self.output_adder
             
-    def muxINC(self,INC_select):
+    def muxINC(self,INC_select, imm):
         if INC_select==0 :
             self.inputB_adder = self.__constantoffset
         else:
-            self.inputB_adder = self._imm
+            self.inputB_adder = imm
 
     def adder(self):
         operandA = int(self._PC,16)
-        operandB = int(self.inputB_adder, 16)
+        operandB = int(self.inputB_adder,16)
         output = operandA+operandB
         self.output_adder = '{:08x}'.format(output)[-8:]
         
@@ -47,6 +46,11 @@ class IAG(HelperFunctions):
 
     def updatePC_temp(self):
         PC = int(self._PC,16)
-        PC = PC + 4
-        self._PC_temp = '{:08x}'.format(PC)[-8:]
+        PC+=4
+        self._PC_Temp = '{:08x}'.format(PC)[-8:]
 
+    def AUIPC(self, imm): #this function is jugaad
+        PC = int(self._PC,16)
+        PC += self.hexToDec(imm)
+        print("AUIPC", '{:08x}'.format(PC)[-8:])
+        return '{:08x}'.format(PC)[-8:]

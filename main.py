e2c0cda184bbd67e73e7d65fb928d460a84b9f9b
Aneeket <aneeketmangal@gmail.com>
2021-04-07 00:56:59 +0530

    finalised

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from GUI.mainPage import *
-
+import sys
+import os
 if __name__ == '__main__':
-    App = QApplication(sys.argv)
-    App.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
-    window = mainScreen(App)
-    sys.exit(App.exec_())
+    n = len(sys.argv)
+    print("Compiling!!!!")
+    print("Check generated folder for details.")
+    if(n == 2 and sys.argv[1] == '2'):
+        directoryPath = os.getcwd()
+        currFilePath = os.path.join(directoryPath, "test", "main.mc")
+        link = frontBackEndInteraction(directoryPath)
+        link.runProgram(currFilePath)
         
+    elif(n == 3 and sys.argv[1] == '2'):
+        directoryPath = os.getcwd()
+        currFilePath = os.path.join(directoryPath, "test", sys.argv[2])
+        link = frontBackEndInteraction(directoryPath)
+        link.runProgram(currFilePath)
+        
+    else:
+        App = QApplication(sys.argv)
+        App.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
+        window = mainScreen(App)
+        sys.exit(App.exec_())
+        
+        
+   

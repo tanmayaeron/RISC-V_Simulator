5ae3d679b02216cc16c78049c42273a2c292788c
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2021-04-16 01:04:34 +0530

    temporary version of BTB

---
+++ BTB.py
@@ -0,0 +1,26 @@
+class BTB:
+
+    """
+        0 -> Not Taken
+        1 -> Taken
+        PC is a String of HEX (without 0x)
+        target is the target address if Taken
+    """
+
+    def __init__(self):
+        lookup = {}
+        pass
+
+    def insert(self, PC, target):
+        if PC not in self.lookup.keys():
+            self.lookup[PC] = [0, target]
+
+	def predict(self, PC):
+		return self.lookup[PC][0]
+
+	def getTarget(self, PC):
+		return self.lookup[PC][1]
+
+    def changeState(self, PC, Outcome):
+        if Outcome != self.lookup[PC][0]:
+            self.lookup[PC][0] = 1 - self.lookup[PC][0]

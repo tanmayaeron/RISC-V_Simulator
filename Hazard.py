58f95e1a84c7865613f41b522f189c3a2c4db933
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2021-04-16 03:58:31 +0530

    changes needed

--- Hazard.py
+++ Hazard.py
@@ -1,12 +1,39 @@
-
 from collections import defaultdict
 
+"""
+    normal
+    normal
+
+    sw
+    normal
+
+    lw
+    normal
+
+lw x10 add
+sw x10 add //hazard
+
+
+    lw  x10 add // addi x10 x10 x10
+    sw x10 adress2 M->M
+
+    addi x10 
+    lw x10 add //1 stalling
+    sw x11 0(x10)
+
+        sw x10 add //no hazard
+        lw x11 add
+"""
 
 class HDU:
     """
     we are making dictionaries for cycle as key 
     F->IR
     D->rd -> number
+    6 -> lw..
+    (6,cycle) = 1
+    7 -> sw..
+    (7,cycle) = 1
     E->RZ -> number, change type from string
     ME->RY -> number, change type from string 
     WB->rd
@@ -15,45 +42,28 @@ class HDU:
     def __init__(self):
         self.dict = defaultdict(int)
 
-    def isHazard(self, cycle, rs1=-1, rs2=-2, rd=-3):
-        # don't call for branch instruction beq, blt
+    def isHazard(self, cycle, rs1 = -1, rs2 = -2, rd = -3): #don't call for branch instruction beq, blt
         if rd == 0:
             return False  # no hazard as x0 register
-        # (stage, cycle number)
-        rdprev1 = self.dict[(2, cycle-1)] if self.dict[(2, cycle-1)] else -4
-        rdprev2 = self.dict[(2, cycle-2)] if self.dict[(2, cycle-2)] else -5
+        
+        if self.dict[(6,cycle-1)] and (self.dict[(2,cycle-1)] == rs2 and rs2):
+            return True
+        
+        rdprev1 = self.dict[(2,cycle-1)] if self.dict[(2,cycle-1)] else -4 # (stage, cycle number)
+        rdprev2 = self.dict[(2,cycle-2)] if self.dict[(2,cycle-2)] else -5
         return False if len(set([rs1, rs2, rdprev1, rdprev2])) == 4 else True
 
-    def isAfterLoadHazard(self, cycle, rs1=-1, rs2=-1, rd=-3):
-        # load then store hazard
-        """
-        li x12 0x1000000
-        ld x11,0(x12)
-        addi x11,x11,x11
-        """
-        if rd == 0:
-            return False  # no hazard as x0 register
-        rdprev1 = self.dict[(2, cycle-1)] if self.dict[(2, cycle-1)] else -4
-        rdprev2 = self.dict[(2, cycle-2)] if self.dict[(2, cycle-2)] else -5
-        rdprev3 = self.dict[(2, cycle-3)] if self.dict[(2, cycle-3)] else -6
-        return False if len(set([rs1, rs2, rdprev1, rdprev2, rdprev3])) == 5 else True
-
-    def isStoreLoadHazard(self, cycle, rs1=-1, rs2=-1, rd=-3):
-        """
-        sw x11,0(x12)
-        lw x12,0(x12)
-        """
-        if rd == 0:
-            return False  # no hazard as x0 register
-
-        pass
-
-    def set_hdu(self, cycle, stage, result):
-
-        self.dict[(stage, cycle)] = result if type(result) == str else result = int(result, 16)
+    def set_hdu(self, cycle, stage, result, ifload = False, ifstore = False):
+        if type(result) == str:
+            result = int(result, 16)
+        if ifload:
+            self.dict[(6,cycle)] = 1
+        if ifstore:
+            self.dict[(7,cycle)] = 1
+        self.dict[(stage, cycle)] = result
 
     def RAW(self):
-        # M->E
-        # Eend->Estart
-        # M->M
+        #M->E
+        #Eend->Estart
+        #M->M
         pass

ca18de735f4e29ed989051487a3c83f37915b8ea
Het <62541263+HETFADIA@users.noreply.github.com>
2021-04-16 04:09:37 +0530

    Fixed self in init

--- PipelineBuffer.py
+++ PipelineBuffer.py
@@ -1,15 +1,17 @@
+from collections import defaultdict
+
+
 class Buffer:
-    
+
     def __init__(self):
-        dict = {}
+        self.dict = defaultdict(int)
 
     def set(self, register_name, register_value):
         self.dict[register_name] = register_value
 
     def get(self, register_name):
-        if register_name in self.dict.keys():
+        if register_name in self.dict:
             return self.dict[register_name]
-        return None
-    
+
     def flush(self):
         self.dict.clear()

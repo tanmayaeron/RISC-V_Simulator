4dadced1a493f2e58955631116196497baf93dfe
Tanmay Aeron <2019csb1124@iitrpr.ac.in>
2021-04-18 17:08:15 +0530

    updated BTB

---
+++ BTB.py
@@ -0,0 +1,24 @@
+class BTB:
+    
+    def __init__(self):
+        self.lookup = {}
+        self.maxlimit = 32
+
+    # prediction - 1 -> Taken
+    # prediction - 0 -> Not Taken
+    # type_ins - 0-> jump
+    #            1 -> branch
+    def add_ins(self,PC,target,type_ins,sign):
+        num_instructions = len(self.lookup.keys())
+        if sign==1:
+            prediction = 1
+        else:
+            prediction = 0
+        if num_instructions < self.maxlimit:
+            self.lookup[PC]=[type_ins,target,prediction]
+            
+    def check_ins(self,PC):
+        if PC in self.lookup.keys():
+            return self.lookup[PC]
+        else:
+            return [-1]*3

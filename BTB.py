8ab3edd5934c668d0b1c8024fa1600dc9f5021bd
Het <62541263+HETFADIA@users.noreply.github.com>
2021-04-22 04:22:46 +0530

    fixed bug in memory.py

--- BTB.py
+++ BTB.py
@@ -9,13 +9,13 @@ class BTB:
     # type_ins - 0-> jump
     #            1 -> branch
     def add_ins(self,PC,target,type_ins,sign):
-        num_instructions = len(self.lookup.keys())
+        num_instructions = len(self.lookup)
         prediction = sign
         if num_instructions < self.maxlimit:
             self.lookup[PC]=[type_ins,target,prediction]
 
     def check_ins(self,PC):
-        if PC in self.lookup.keys():
+        if PC in self.lookup:
             return self.lookup[PC]
         else:
             return [-1]*3

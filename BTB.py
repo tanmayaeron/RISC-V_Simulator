dcaf9d57735262b3b5c9b42d1f4ea5365e7a774b
Aditya Agarwal <2019csb1064@iitrpr.ac.in>
2021-04-22 22:39:32 +0530

    Update BTB.py

--- BTB.py
+++ BTB.py
@@ -1,21 +1,47 @@
+from collections import defaultdict
+
+
 class BTB:
 
     def __init__(self):
-        self.lookup = {}
-        self.maxlimit = 32
+        self.lookup = defaultdict(int)
+        
+        #  branch and jal instruction in self.lookup
+        
+        self.predicted = defaultdict(int)
+        # stores True if taken else False
 
     # prediction - 1 -> Taken
     # prediction - 0 -> Not Taken
-    # type_ins - 0-> jump
-    #            1 -> branch
-    def add_ins(self,PC,target,type_ins,sign):
-        num_instructions = len(self.lookup)
-        prediction = sign
-        if num_instructions < self.maxlimit:
-            self.lookup[PC]=[type_ins,target,prediction]
+    # isBTB for jump and branch
+    # S_Select  for branch
+    def isImmediatePositive(self,imm):
+        # returns 1 if imm is greater than 0
+        # imm is in hex and sign extended
+        return int(imm[0],16)>=8
 
-    def check_ins(self,PC):
-        if PC in self.lookup:
-            return self.lookup[PC]
-        else:
-            return [-1]*3
+    def predict(self, PC ):
+        
+        return [True, self.lookup[PC]] if PC in self.lookup else [False, "0"*8]
+
+    def HitMiss(self, RZ, PC):
+        # prediction 
+        if self.lookup[PC] == RZ:
+            return True
+        return False
+
+    def addInstruction(self, PC, PC_temp, imm, target, S_Select, isBTB):
+        if PC not in self.lookup:
+
+            if S_Select:
+                self.lookup[PC] = PC_temp if self.isImmediatePositive(imm) else target
+                self.predicted[PC] = False if self.isImmediatePositive(imm) else True
+                # if it is in lookup and positive then we predict it as False and take the next instruction(pc_temp)
+                # else we predict it as true and and take the target
+            elif isBTB:
+                self.lookup[PC] = target
+                self.predicted[PC] = True
+                # it is jal instruction and so we take the target
+
+    def ifSetInstruction(self,PC):
+        return PC in self.lookup
